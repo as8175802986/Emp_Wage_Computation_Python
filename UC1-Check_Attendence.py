4d8fb8dd76361c7ecf790ac924d7e61730a12845
Arunesh sharma <73111126+as8175802986@users.noreply.github.com>
2022-03-08 23:26:10 +0530

    Update UC1-Check_Attendence.py

--- UC1-Check_Attendence.py
+++ UC1-Check_Attendence.py
@@ -1,9 +1,11 @@
-def Attendence():
-    import random
-    number = random.randint(0,1)
+import random
+def attendence():
+    number = random.randint(0, 1)
     if number == 1:
         print("Employee is present")
     else:
         print("Employee is absent")
+
 #Calling function to print result
-Attendence()
+if __name__== '__main__':
+    attendence()
